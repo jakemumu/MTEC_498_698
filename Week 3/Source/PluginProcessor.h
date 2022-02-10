dc98bb692b6e7965baf1648a075e86c455d5e858
Jacob Penn <penn.jacob@gmail.com>
2022-02-10 14:42:12 -0600

    move sinewave into it's own class

--- Week 3/Source/PluginProcessor.h
+++ Week 3/Source/PluginProcessor.h
@@ -10,82 +10,8 @@
 
 #include <JuceHeader.h>
 
-
-/* THIS IS A BASIC SINEWAVE CLASS */
-
-class SineWave {
-    
-// Public indicates that these methods will be available to the outside program
-    
-// ex SineWave.initialize(554, 44100)
-public:
-    
-    /* CONSTRUCTOR
-     
-     A constructor is called when a class is created, it can be used to allocate memory
-     or setup various internal data of the class
-     
-     */
-    SineWave() {
-        // EMPTY
-    }
-    
-    
-    /* DECONSTRUCTOR
-     
-     A deconstructor is called when a class is delete, it can be used to free memory
-     or delete things we'll no longer be using in the application
-     
-     */
-    ~SineWave() {
-        // EMPTY
-    }
-    
-    /* INITALIZE
-     
-     We call this initialze function from our prepare to play method to get our
-     SineWave ready for playback
-     
-     */
-    void initialize(float inFrequencyHz, float inSampleRate) {
-        
-        // Set our desired frequency in HZ
-        mFreqHz = inFrequencyHz;
-        
-        // Set our samplerate for math calculations
-        mSampleRate = inSampleRate;
-    }
-    
-    /* */
-    float getNextSample() {
-        
-        // calculate our sine function output -- using a sin call scaled to repete between 0 - 1 on the input
-        float output = std::sin(juce::MathConstants<float>::twoPi * mPhase);
-        
-        // move our phase forward in the sign table by a single step determined by our desired samplerate & playback hz
-        mPhase += mFreqHz / mSampleRate;
-        
-        // if we go passed 1 -- lets loop back around the sine wave
-        if (mPhase > 1.f) {
-            mPhase -= 1.f;
-        }
-        
-        // return the output to the caller
-        return output;
-    }
-    
-    
-// Private means these are not accessible to the outside world
-    
-// ex: SineWave.mFreqHz <- Illegal and impossible to access a private member variable
-private:
-    
-    // These are member variables of the class (internal data)
-    float mFreqHz = 442;
-    float mSampleRate = 44100;
-    float mPhase = 0;
-    
-};
+// INCLUDE OUR SINEWAVE
+#include "SineWave.h"
 
 //==============================================================================
 /**
