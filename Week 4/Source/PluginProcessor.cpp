32229e025ee8c08b62a793eb63e6c9497ea96897
Jacob Penn <penn.jacob@gmail.com>
2022-02-15 11:30:33 -0600

    add smoothed value for week 4

---
+++ Week 4/Source/PluginProcessor.cpp
@@ -0,0 +1,206 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+CoursePluginAudioProcessor::CoursePluginAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+CoursePluginAudioProcessor::~CoursePluginAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String CoursePluginAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool CoursePluginAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool CoursePluginAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool CoursePluginAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double CoursePluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int CoursePluginAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int CoursePluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void CoursePluginAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String CoursePluginAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void CoursePluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void CoursePluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Initialize our sine wave
+    mSineWave.initialize(442, sampleRate);
+}
+
+void CoursePluginAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool CoursePluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void CoursePluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    // Left channel = 0 in the audio buffer
+    int left = 0;
+    
+    // Right channel = 1 in the audio buffer
+    int right = 1;
+    
+    // FOR EACH SAMPLE IN THE INCOMING AUDIO BUFFER
+    for (int sample_index = 0; sample_index < buffer.getNumSamples(); sample_index++) {
+        
+        // GET THE NEXT SAMPLE FROM OUR SINE GENERATOR
+        float output = mSineWave.getNextSample();
+                
+        // STORE THE OUTPUT TO THE LEFT AND RIGHT CHANNELS OF THE AUDIO BUFFER
+        
+        // FROM JUCE:
+        // void setSample (int destChannel, int destSample, Type newValue)
+        
+        buffer.setSample(left, sample_index, output);
+        
+        buffer.setSample(right, sample_index, output);
+        
+    }
+}
+
+SineWave* CoursePluginAudioProcessor::getSineWave1()
+{
+    return &mSineWave;
+}
+
+//==============================================================================
+bool CoursePluginAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* CoursePluginAudioProcessor::createEditor()
+{
+    return new CoursePluginAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void CoursePluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void CoursePluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new CoursePluginAudioProcessor();
+}
---
+++ Week 4/Source/PluginProcessor.h
@@ -0,0 +1,86 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+// INCLUDE OUR SINEWAVE
+#include "SineWave.h"
+
+//==============================================================================
+/**
+*/
+class CoursePluginAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    CoursePluginAudioProcessor();
+    ~CoursePluginAudioProcessor() override;
+
+    //==============================================================================
+    
+    /*
+     
+     This function is called before audio playback in order to initialize objects which well use during
+     audio processing.
+       
+     It will be called a single time, and then again if the user changes sample rates, block size, or
+     does a bounce / export of the audio.
+     
+     */
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    
+    /*
+     
+     This function will be called continuously during playback, and is where we can modify
+     or generate audio signals.
+     
+     */
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    
+    /* pointer to sine wave */
+    SineWave* getSineWave1();
+    
+    /* called before destruction to free data (rarely used with modern cpp) */
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    
+    SineWave mSineWave;
+    
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CoursePluginAudioProcessor)
+};
